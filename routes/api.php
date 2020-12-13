7a365aed445c1007a2ed5990011b637795ce4804
kaneki-neth <kaneki-neth@gmail.com>
2020-12-13 23:58:45 +0800

    3rd commit for the CRUD project

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\CategoryController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -17,3 +18,8 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+Route::get("categories", [CategoryController::class, 'index']);
+Route::post("categories", [CategoryController::class, 'store']);
+Route::get("categories/{category}", [CategoryController::class, 'show']);
+Route::put("categories/{category}", [CategoryController::class, 'update']);
+Route::delete("categories/{category}", [CategoryController::class, 'destroy']);
